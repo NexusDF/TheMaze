cf865c5a28f1ab8de680f4f53368e370b9a783b9
NexusDF <40206725+NexusDF@users.noreply.github.com>
2019-02-03 16:51:29 +0400

    Add Armature and Idle animation

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMotor : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+
+    private CharacterController _char;
+
+    private float _MovX;
+    private float _MovY;
+
+    void Start()
+    {
+        _char = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        _MovX = Input.GetAxis("Horizontal");
+        _MovY = Input.GetAxis("Vertical");
+
+        Vector3 movHorizontal = transform.right * _MovX;
+        Vector3 movVertical = transform.forward * _MovY;
+
+        Vector3 moveVector = (movHorizontal + movVertical).normalized * moveSpeed * Time.deltaTime;
+
+        _char.Move(moveVector);
+    }
+
+}

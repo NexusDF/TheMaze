a66e4124cef18f774a0d6714224c6a9f329be4a2
NexusDF <40206725+NexusDF@users.noreply.github.com>
2019-02-03 18:13:41 +0400

    Add the MazeCreator

---
+++ Assets/Scripts/MazeCreator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+public class MazeCreator : MonoBehaviour
+{
+    public Texture2D map;
+
+    public ColorToPrefab[] colorMappings;
+
+    void Start()
+    {
+        GeneratorMaze();
+    }
+
+    void GeneratorMaze()
+    {
+        for (int x = 0; x < map.width; x++)
+        {
+            for (int y = 0; y < map.height; y++)
+            {
+                GeneratorTile(x, y);
+            }
+        }
+    }
+
+    void GeneratorTile(int x, int y)
+    {
+        Color pixelColor = map.GetPixel(x, y);
+
+        if (pixelColor.a == 0)
+        {
+            return;
+        }
+
+        foreach (ColorToPrefab colorMapping in colorMappings)
+        {
+            if (colorMapping.color.Equals(pixelColor))
+            {
+                Vector3 position = new Vector3(x + 0.5f, 0, y + 0.5f);
+                Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+
+    }
+}

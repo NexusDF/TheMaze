4c667e27c676f5fffbf1121ffab6521aeeb201d5
lnquisitor <45894064+lnquisitor@users.noreply.github.com>
2019-02-11 14:32:10 +0400

    15

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    BoxCollider col;
+    public Manager manager;
+    public int number;
+    public int NumberCell;
+
+    void Start()
+    {
+        col = GetComponent<BoxCollider>();
+    }
+
+    private void OnMouseDown()
+    {
+        if(!manager.isWin)
+        {
+            col.enabled = false;
+            RaycasHit hit;
+            if (!Physics.Linecast(transform.position, transform.position + transform.right, out hit))
+            {
+                transform.position = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+            }
+            else if (!Physics.Linecast(transform.position, transform.position + -transform.right, out hit))
+            {
+                transform.position = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+            }
+            else if (!Physics.Linecast(transform.position, transform.position + transform.forward, out hit))
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
+            }
+            else if (!Physics.Linecast(transform.position, transform.position + -transform.forward, out hit))
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
+            }
+            col.enabled = true;
+        }
+    }
+
+    private void OnTriggerEnter()
+    {
+        if (other.tag == "trigger")
+        {
+            NumberCell = other.transform.GetComponent<NumberCell>().NumberCell;
+        }
+    }
+}

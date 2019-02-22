e99f266813298fb1048a30b11d7ac8927b55cc31
lnquisitor <45894064+lnquisitor@users.noreply.github.com>
2019-02-22 16:46:18 +0400

    Win in bricks

--- Assets/Scripts/Bricks.cs
+++ Assets/Scripts/Bricks.cs
@@ -8,9 +8,13 @@ public class Bricks : MonoBehaviour
     private int x, y;
     public Material _Blue;
     public Material _Red;
+    private bool win = false;
+    public GameObject WinPanel;
+    private int[] WinArray = { 0, 2, 4, 7, 9, 11, 12, 14, 16, 19, 21, 23, 24, 26, 28, 31, 33, 35};
 
     private void Start()
     {
+        WinPanel.SetActive(false);
         for (int i = 0; i < brick.Length; i++)
         {
             brick[i].x = i / count;
@@ -20,22 +24,55 @@ public class Bricks : MonoBehaviour
 
     private void OnMouseDown()
     {
-        brick[x * count + y].flag = !brick[x * count + y].flag;
-        for (int i = 0; i < count; i++)
+        if (!win)
         {
-            brick[i * count + y].flag = !brick[i * count + y].flag;
-        }
-        for (int i = 0; i < count; i++)
-        {
-            brick[x * count + i].flag = !brick[x * count + i].flag;
-        }
-        for (int i = 0; i < brick.Length; i++)
-        {
-            if (brick[i].flag == true)
+            brick[x * count + y].flag = !brick[x * count + y].flag;
+            for (int i = 0; i < count; i++)
             {
-                brick[i].GetComponent<MeshRenderer>().material = _Red;
+                brick[i * count + y].flag = !brick[i * count + y].flag;
             }
-            else brick[i].GetComponent<MeshRenderer>().material = _Blue;
+            for (int i = 0; i < count; i++)
+            {
+                brick[x * count + i].flag = !brick[x * count + i].flag;
+            }
+            for (int i = 0; i < brick.Length; i++)
+            {
+                if (brick[i].flag == true)
+                {
+                    brick[i].GetComponent<MeshRenderer>().material = _Red;
+                }
+                else brick[i].GetComponent<MeshRenderer>().material = _Blue;
+            }
+            Check();
+        }
+    }
+
+    private void Check()
+    {
+        bool check = false;
+        bool _true = true;
+        bool _false = false;
+        for (int j = 0; j < brick.Length; j++)
+        {
+            for (int i = 0; i < WinArray.Length; i++)
+            {
+                if (j == WinArray[i])
+                {
+                    check = true;
+                    break;
+                }
+            }
+            if (check) _true = _true & brick[j].flag;
+            else _false = _false | brick[j].flag;
+            check = false;
+        }
+        if (_true == true && _false == false)
+        {
+            for (int i = 0; i < brick.Length; i++)
+            {
+                brick[i].win = true;
+            }
+            WinPanel.SetActive(true);
         }
     }
 }
